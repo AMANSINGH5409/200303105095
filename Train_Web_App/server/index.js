3e9ccf50ccc5e95031e6924a6dd0e1a37819d190
AMAN RAJAN SINGH <madhusingh74055@gmail.com>
2023-07-13 11:42:31 +0530

    modules added

--- Train_Web_App/server/index.js
+++ Train_Web_App/server/index.js
@@ -10,4 +10,5 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.listen(PORT, () => console.log(`Server Running on ${PORT}`));
+app.listen(process.env.PORT);
+console.log("Server Started.....");
